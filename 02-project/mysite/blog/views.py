0a9d02bda5236e2165e79959a3f4dfd40f96ea88
zhanglongde <1039326495@qq.com>
2018-03-10 01:18:07 +0800

    django内置模板渲染 Navicat

--- 02-project/mysite/blog/views.py
+++ 02-project/mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse
-
+import time
 # Create your views here.
 
 def show_time(req):
-    return HttpResponse("hello")
+    # return HttpResponse("hello")
+    t=time.ctime()
+    return render(req,"index.html", {"time": t})
